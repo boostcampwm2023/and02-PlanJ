e07b37dfe4a98b3f07bb13f0f812d06c90ae5b75
신유빈 <101437398+yubin0727@users.noreply.github.com>
2023-11-14 13:57:55 +0900

    Feat: 로그인 액티비티 네비게이션 그래프 추가
    로그인 액티비티, 로그인/회원가입 프래그먼트 추가
    로그인 액티비티에 FragmentContainerView 추가 및 NavHost 추가

---
+++ PlanJ/app/src/main/java/com/boostcamp/planj/ui/login/SignUpFragment.kt
@@ -0,0 +1,18 @@
+package com.boostcamp.planj.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.boostcamp.planj.R
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+}
