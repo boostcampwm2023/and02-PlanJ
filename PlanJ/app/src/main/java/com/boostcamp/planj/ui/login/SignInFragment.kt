f10b620d4951f780c09334ecbe6ac93b65fbf317
신유빈 <101437398+yubin0727@users.noreply.github.com>
2023-11-14 15:40:15 +0900

    Feat: 로그인 화면(SignInFragment) xml 구현
    LoginFragment -> SignInFragment로 이름 변경
    drawable - 버튼 모양 및 화면 내 아이콘,이미지 추가
    SignIn 프래그먼트 xml 구현
    colors, strings에 관련 리소스 추가

--- PlanJ/app/src/main/java/com/boostcamp/planj/ui/login/SignInFragment.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/ui/login/SignInFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.boostcamp.planj.R
 
-class LoginFragment : Fragment() {
+class SignInFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_signin, container, false)
     }
 }
