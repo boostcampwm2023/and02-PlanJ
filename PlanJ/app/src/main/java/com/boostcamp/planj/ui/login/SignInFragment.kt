e482d3a806aef47fc11523a715d0fc97137f304a
신유빈 <101437398+yubin0727@users.noreply.github.com>
2023-11-14 15:56:32 +0900

    Feat: 로그인 화면에서 회원가입 화면 이동
    NavController 이용해 SignInFragment->SignUpFragment 이동 구현
    login_nav에서 id 수정

--- PlanJ/app/src/main/java/com/boostcamp/planj/ui/login/SignInFragment.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/ui/login/SignInFragment.kt
@@ -5,14 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.boostcamp.planj.R
+import com.boostcamp.planj.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+    ): View {
+        binding = FragmentSignInBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.btnLoginSignup.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_signUpFragment2)
+        }
+    }
+
 }
