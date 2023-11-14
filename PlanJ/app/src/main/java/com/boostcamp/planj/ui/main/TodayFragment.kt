bd999fc830758e33a16edc739c9399888e43511b
윤찬 <vsvx13@naver.com>
2023-11-14 15:43:54 +0900

    Feat: viewPager, TabLayout 연동하기 메인 홈화면 Today와 Week 화면을 뷰페이저를 통해 스와이프로 변경이 가능하며, 상단에 탭을 눌러 화면 전환을 할 수 있도록 구현

--- PlanJ/app/src/main/java/com/boostcamp/planj/ui/main/TodayFragment.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/ui/main/TodayFragment.kt
@@ -5,21 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.boostcamp.planj.databinding.FragmentHomeBinding
-import com.google.android.material.tabs.TabLayoutMediator
+import com.boostcamp.planj.databinding.FragmentListBinding
+import com.boostcamp.planj.databinding.FragmentTodayBinding
 
-class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
+class TodayFragment: Fragment() {
+    private var _binding: FragmentTodayBinding? = null
     private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.vpMain.adapter = MyFragmentPagerAdapter(this)
-        val title = listOf("Today", "Week")
-        TabLayoutMediator(binding.tabLayout, binding.vpMain) { tab, position ->
-            tab.text = title[position]
-        }.attach()
     }
 
     override fun onCreateView(
@@ -27,7 +21,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentTodayBinding.inflate(inflater, container, false)
         return binding.root
     }
 
