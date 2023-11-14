b1c3c7ad81cdb7ddc0a01d1816a21a0818046721
Junsoo <junsoo2018@naver.com>
2023-11-15 00:33:10 +0900

    Feat: dotenv 패키지 설치 및 적용

--- be/src/config/typeorm.config.ts
+++ be/src/config/typeorm.config.ts
@@ -1,4 +1,7 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: "mysql",
