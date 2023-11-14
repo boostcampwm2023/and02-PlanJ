6932302241c6ea66813ccd5fb6b9bcc40e004e0e
신유빈 <101437398+yubin0727@users.noreply.github.com>
2023-11-15 01:10:25 +0900

    Feat: 회원가입 화면 유효성 검증 추가
    사용자 입력에 따라 이메일, 비밀번호, 닉네임 체크하여 각각의 상태 변경

--- PlanJ/app/src/main/java/com/boostcamp/planj/ui/login/SignUpViewModel.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/ui/login/SignUpViewModel.kt
@@ -22,4 +22,51 @@ class SignUpViewModel : ViewModel() {
 
     private val _nicknameState = MutableLiveData<NicknameState>()
     val nicknameState: LiveData<NicknameState> = _nicknameState
+
+    private val regexEmail = Regex("""^([A-z0-9_\-.]+)@([A-z0-9_\-]+)\.([a-zA-Z]{2,5})$""")
+    private val regexEnglish = Regex("""[A-z]""")
+    private val regexNum = Regex("""[0-9]""")
+    private val regexSpecial = Regex("""[~`!@#\\$%^&*()_\-+=]""")
+
+    fun checkEmail() {
+        userEmail.value?.let { userEmail ->
+            _emailState.value =
+                if (regexEmail.matches(userEmail)) EmailState.AVAILABLE else EmailState.ERROR_FORMAT
+        }
+    }
+
+    fun checkPwd() {
+        userPwd.value?.let { userPwd ->
+            _pwdState.value = if (userPwd.length < 8 || userPwd.length > 16) {
+                PwdState.ERROR_LENGTH
+            } else {
+                if (!userPwd.contains(regexEnglish)) PwdState.ERROR_ENGLISH
+                else if (!userPwd.contains(regexNum)) PwdState.ERROR_NUM
+                else if (!userPwd.contains(regexSpecial)) PwdState.ERROR_SPECIAL
+                else PwdState.AVAILABLE
+            }
+
+            userPwdConfirm.value?.let { userPwdConfirm ->
+                if (userPwdConfirm.isNotEmpty()) {
+                    _pwdConfirmState.value =
+                        if (userPwd == userPwdConfirm) PwdConfirmState.AVAILABLE else PwdConfirmState.ERROR
+                }
+            }
+        }
+    }
+
+    fun checkPwdConfirm() {
+        _pwdConfirmState.value =
+            if (!userPwdConfirm.value.isNullOrEmpty() && !userPwd.value.isNullOrEmpty() && userPwdConfirm.value == userPwd.value)
+                PwdConfirmState.AVAILABLE
+            else
+                PwdConfirmState.ERROR
+    }
+
+    fun checkNickname() {
+        userNickname.value?.let { userNickname ->
+            _nicknameState.value =
+                if (userNickname.length in 1..12) NicknameState.AVAILABLE else NicknameState.ERROR_LENGTH
+        }
+    }
 }
