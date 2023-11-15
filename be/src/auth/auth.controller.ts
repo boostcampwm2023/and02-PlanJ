bb0712d0846fa8bebbc9e64e0ef6a046ee3effcc
Junsoo <junsoo2018@naver.com>
2023-11-15 15:11:33 +0900

    Feat: 회원 탈퇴 기능 구현
    회원 탈퇴 기능을 @DeleteDateColumn을 이용하여 논리적 삭제

--- be/src/auth/auth.controller.ts
+++ be/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Post, Body, ValidationPipe, HttpCode, HttpStatus} from "@nestjs/common";
+import { Controller, Post, Body, HttpCode, HttpStatus, Delete } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UserLoginDto } from "./dto/user-login.dto";
 import { CreateUserDto } from "./dto/create-user.dto";
@@ -17,6 +17,12 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async login(@Body() dto: UserLoginDto): Promise<JSON> {
     const result = await this.authService.login(dto);
+    return JSON.parse(result);
+  }
+
+  @Delete("/delete")
+  async deleteAccount(@Body() dto: UserLoginDto) {
+    const result = await this.authService.deleteAccount(dto);
     return JSON.parse(result);
   }
 }
