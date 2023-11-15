20af51f68e547387c015de3d551b2d6fd0192988
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-16 01:34:50 +0900

    Feat: ConfigModule 추가
    ConfigModule을 이용하여 동적으로 환경 변수 관리하는 부분 구현

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -3,11 +3,26 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { typeORMConfig } from "./config/typeorm.config";
+import { TypeOrmConfigService } from "./config/typeorm.config";
 import { ScheduleModule } from "./schedule/schedule.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import dbConfig from "./config/dbConfig";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule, ScheduleModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${__dirname}/config/env/.${process.env.NODE_ENV}.env`,
+      load: [dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => TypeOrmConfigService.createTypeOrmOptions(configService),
+    }),
+    AuthModule,
+    ScheduleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
