7f353ebcb8e90ae935052de33a4334f391c6e8d0
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-15 15:14:26 +0900

    Feat: create user modify dto
    사용자가 개인정보를 수정할 때 사용할 dto 생성
    추후 profile image 추가 예정

---
+++ be/src/auth/dto/user-modify.dto.ts
@@ -0,0 +1,12 @@
+import {IsEmail, IsString, MaxLength, MinLength} from "class-validator";
+
+export class UserModifyDto {
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(2)
+    @MaxLength(12)
+    nickname: string;
+}
