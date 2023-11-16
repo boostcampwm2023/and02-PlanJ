90336af6cce3498c7bd7a749e33e6211177cab0a
Junsoo <junsoo2018@naver.com>
2023-11-16 17:36:54 +0900

    Feat: ScheduleMeta 테이블과 Schedule 테이블 모두에 데이터가 저장
    ScheduleMeta 테이블과 Schedule 테이블 모두에 데이터가 저장되도록 구현

--- be/src/schedule/schedule.repository.ts
+++ be/src/schedule/schedule.repository.ts
@@ -1,42 +1,42 @@
 import { DataSource, Repository } from "typeorm";
 import { AddScheduleDto } from "./dto/add-schedule.dto";
-import { ScheduleMetaEntity } from "./entity/schedule.meta.entity";
-import { HttpResponse } from "src/utils/http.response";
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { ulid } from "ulid";
+import { ScheduleEntity } from "./entity/schedule.entity";
+import { HttpResponse } from "src/utils/http.response";
 
 @Injectable()
-export class ScheduleRepository extends Repository<ScheduleMetaEntity> {
+export class ScheduleRepository extends Repository<ScheduleEntity> {
   constructor(dataSource: DataSource) {
-    super(ScheduleMetaEntity, dataSource.createEntityManager());
+    super(ScheduleEntity, dataSource.createEntityManager());
   }
 
-  async addSchedule(dto: AddScheduleDto): Promise<string> {
-    const { userId, title, description, startTime, endTime } = dto;
+  async addSchedule(dto: AddScheduleDto) {
+    const { startAt, endAt } = dto;
 
-    const metaId = ulid();
-    const schedule = this.create({ metaId, title, description, startTime, endTime });
-
-    console.log("ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ");
-    console.log(schedule);
-    console.log("ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ");
+    const scheduleId = ulid();
+    const schedule = this.create({
+      scheduleId,
+      startAt,
+      endAt,
+      finished: false,
+      failed: false,
+      remindMemo: "",
+      last: true,
+    });
 
     try {
-      console.log("ㅌㅌㅌㅌㅌㅌㅌㅌㅌㅌㅌㅌㅌㅌㅌ");
       await this.save(schedule);
       const body: HttpResponse = {
         message: "일정 추가 성공",
         statusCode: 200,
         data: {
-          pk: metaId,
+          scheduleId: scheduleId,
         },
       };
 
-      console.log(body);
-
       return JSON.stringify(body);
     } catch (error) {
-      console.log(error.message);
       throw new InternalServerErrorException();
     }
   }
