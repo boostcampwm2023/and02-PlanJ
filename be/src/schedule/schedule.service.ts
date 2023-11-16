0f0e66c4f0abd6ee01c2a3cde694c61c2e421ea5
Junsoo <junsoo2018@naver.com>
2023-11-16 15:45:59 +0900

    Feat: addSchedule 초안 작성

--- be/src/schedule/schedule.service.ts
+++ be/src/schedule/schedule.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { AddScheduleDto } from "./dto/add-schedule.dto";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class ScheduleService {
-    add() {
+  constructor(
+    @InjectRepository(ScheduleRepository)
+    private scheduleRepository: ScheduleRepository,
+  ) {}
 
-    }
+  addSchedule(dto: AddScheduleDto) {
+    return this.scheduleRepository.addSchedule(dto);
+  }
 
-    getDaily() {
+  getDaily() {}
 
-    }
-
-    getWeekly() {
-
-    }
+  getWeekly() {}
 }
