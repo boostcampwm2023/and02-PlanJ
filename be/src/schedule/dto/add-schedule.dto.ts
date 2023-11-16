54722e302275fb788bb21a10eb5d56eff038f803
Junsoo <junsoo2018@naver.com>
2023-11-16 16:39:50 +0900

    Feat: 일정 추가 dto 형식 작성
    일정 추가 dto 형식 작성

--- be/src/schedule/dto/add-schedule.dto.ts
+++ be/src/schedule/dto/add-schedule.dto.ts
@@ -1,6 +1,20 @@
-import { IsString } from "class-validator";
+import { IsDate, IsDateString, IsNotEmpty, IsString, Matches } from "class-validator";
 
 export class AddScheduleDto {
   @IsString()
+  @IsNotEmpty()
   userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @Matches(/^\d{2}:\d{2}:\d{2}$/, { message: "올바른 시간 형식이 아닙니다. (HH:MM:SS)" })
+  startTime: string;
+
+  @Matches(/^\d{2}:\d{2}:\d{2}$/, { message: "올바른 시간 형식이 아닙니다. (HH:MM:SS)" })
+  endTime: string;
 }
