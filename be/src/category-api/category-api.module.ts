74bfe76907f2c86bb35f6809000eec8a931f26cc
Junsoo <junsoo2018@naver.com>
2023-11-20 12:05:48 +0900

    Refactor: schedule-meta-data 모듈 생성 및 의존 관계 설정

--- be/src/category-api/category-api.module.ts
+++ be/src/category-api/category-api.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { CategoryApiController } from "./category-api.controller";
 import { ScheduleModule } from "src/schedule/schedule.module";
 import { AuthModule } from "src/auth/auth.module";
+import { ScheduleMetaDataModule } from "src/schedule-meta-data/schedule-meta-data.module";
 
 @Module({
-  imports: [ScheduleModule, AuthModule],
+  imports: [ScheduleModule, ScheduleMetaDataModule, AuthModule],
   controllers: [CategoryApiController],
 })
 export class CategoryApiModule {}
--- be/src/schedule-api/schedule-api.module.ts
+++ be/src/schedule-api/schedule-api.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ScheduleApiController } from "./schedule-api.controller";
 import { ScheduleModule } from "src/schedule/schedule.module";
+import { ScheduleMetaDataModule } from "src/schedule-meta-data/schedule-meta-data.module";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
-  imports: [ScheduleModule],
+  imports: [ScheduleModule, ScheduleMetaDataModule, AuthModule],
   controllers: [ScheduleApiController],
   providers: [],
 })
