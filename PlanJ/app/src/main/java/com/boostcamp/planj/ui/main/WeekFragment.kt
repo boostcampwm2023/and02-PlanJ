4fa6616563739a8047d9aacbbbd1b00389f1fb94
heyera <63634801+heyera@users.noreply.github.com>
2023-11-21 01:04:25 +0900

    Feat: 주간일정 통계 화면 구성
    주간 달력에 리스트를 사용하는 방식과 사용하는 방식을 개선했습니다.
    밑에 버튼을 추가해서 주간 통계를 보여주는 디자인을 작성해뒀습니다.

--- PlanJ/app/src/main/java/com/boostcamp/planj/ui/main/WeekFragment.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/ui/main/WeekFragment.kt
@@ -1,8 +1,11 @@
 package com.boostcamp.planj.ui.main
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,12 +13,12 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.boostcamp.planj.R
+import com.boostcamp.planj.data.model.Schedule
 import com.boostcamp.planj.databinding.FragmentWeekBinding
 import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalAdjusters
-import java.util.Locale
 
 class WeekFragment : Fragment() {
     private var _binding: FragmentWeekBinding? = null
@@ -25,14 +28,18 @@ class WeekFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initAdapter()
+        val scheduleList = DummySchedule.getDummyList()
+
+        initAdapter(scheduleList)
+        resultSchedule(scheduleList)
+
+
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun initAdapter() {
+    private fun initAdapter(scheduleList: List<Schedule>) {
         lateinit var calendarAdapter: CalendarAdapter
         var calendarList = ArrayList<CalendarVO>()
-        val scheduleList = DummySchedule.getDummyList()
 
 
         var week_day: Array<String> = resources.getStringArray(R.array.calendar_day)
@@ -41,16 +48,15 @@ class WeekFragment : Fragment() {
 
 
         calendarList.run {
-            val dateFormat =
-                DateTimeFormatter.ofPattern("dd").withLocale(Locale.forLanguageTag("ko"))
-            val monthFormat = DateTimeFormatter.ofPattern("yyyy-MM-").withLocale(Locale.forLanguageTag("ko"))
+            val dateFormat = DateTimeFormatter.ofPattern("dd")
+            val monthFormat = DateTimeFormatter.ofPattern("yyyy-MM-")
 
             month = LocalDateTime.now().format(monthFormat).split("-")[1].toInt()
 
             var preSunday: LocalDateTime =
-                LocalDateTime.now().with(TemporalAdjusters.previous(DayOfWeek.SUNDAY))
-            if(LocalDateTime.now().dayOfWeek==DayOfWeek.SUNDAY){
-                preSunday= LocalDateTime.now()
+                (LocalDateTime.now().with(TemporalAdjusters.previous(DayOfWeek.SUNDAY)))
+            if (LocalDateTime.now().dayOfWeek == DayOfWeek.SUNDAY) {
+                preSunday = LocalDateTime.now()
             }
 
             for (i in 0..6) {
@@ -66,10 +72,51 @@ class WeekFragment : Fragment() {
         binding.rvWeekWeek.layoutManager = GridLayoutManager(context, 7)
     }
 
+    private fun resultSchedule(scheduleList: List<Schedule>) {
+
+        val finishList = scheduleList.filter { schedule: Schedule ->
+            schedule.failed.not() && schedule.finished
+        }
+        val countFinish = SpannableString(
+            binding.btnWeekFinish.text.toString().plus(
+                "${finishList.size}"
+            )
+        )
+        countFinish.setSpan(
+            ForegroundColorSpan(Color.GREEN),
+            binding.btnWeekFinish.text.length,
+            countFinish.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.btnWeekFinish.text = countFinish
+
+
+        val failList = scheduleList.filter { schedule: Schedule -> schedule.failed }
+        val countFail =
+            SpannableString(binding.btnWeekFail.text.toString().plus("${failList.size}"))
+        countFail.setSpan(
+            ForegroundColorSpan(Color.RED),
+            binding.btnWeekFail.text.length,
+            countFail.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.btnWeekFail.text = countFail
+
+
+        val haveList = scheduleList.filter { schedule: Schedule -> schedule.finished.not() }
+        val countHave =
+            SpannableString(binding.btnWeekHave.text.toString().plus("${haveList.size}"))
+        countHave.setSpan(
+            ForegroundColorSpan(Color.BLUE),
+            binding.btnWeekHave.text.length,
+            countHave.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.btnWeekHave.text = countHave
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentWeekBinding.inflate(inflater, container, false)
         return binding.root
