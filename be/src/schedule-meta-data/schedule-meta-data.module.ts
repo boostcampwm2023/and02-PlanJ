6bd8d538e37a3f5a2ee7dba4aa99814cd1e3115d
Junsoo <junsoo2018@naver.com>
2023-11-22 20:23:14 +0900

    Style: 사용하지 않는 파일 삭제

--- be/src/schedule-meta-data/schedule-meta-data.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ScheduleMetaDataService } from "./schedule-meta-data.service";
-
-@Module({
-  providers: [ScheduleMetaDataService],
-})
-export class ScheduleMetaDataModule {}
