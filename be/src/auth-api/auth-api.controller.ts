2c8b5d1df88202f575183382c220f4a3ec1fbdaa
famo1245 <kandallee38@gmail.com>
2023-11-23 17:49:52 +0900

    Refactor: service간 책임 분리
    하나의 서비스가 하나의 책임만 가지도록 분리

--- be/src/auth-api/auth-api.controller.ts
+++ be/src/auth-api/auth-api.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Post, Body, HttpCode, HttpStatus, Delete, Patch } from "@nestjs/common";
+import {Controller, Post, Body, HttpCode, HttpStatus, Delete, Patch, UseGuards, Headers} from "@nestjs/common";
 import { UserService } from "../user/user.service";
 import { UserLoginDto } from "../user/dto/user-login.dto";
 import { CreateUserDto } from "../user/dto/create-user.dto";
 import { UserModifyDto } from "../user/dto/user-modify.dto";
-import {AuthApiService} from "./auth-api.service";
-import {HttpResponse} from "../utils/http.response";
+import { AuthApiService } from "./auth-api.service";
+import {AuthGuard} from "../guard/auth.guard";
 
 @Controller("/api/auth")
 export class AuthApiController {
@@ -15,7 +15,7 @@ export class AuthApiController {
 
   @Post("/register")
   async register(@Body() dto: CreateUserDto): Promise<JSON> {
-    const result = await this.userService.register(dto);
+    const result = await this.authApiService.register(dto);
     return JSON.parse(result);
   }
 
@@ -26,16 +26,19 @@ export class AuthApiController {
     return JSON.parse(result);
   }
 
+  @UseGuards(AuthGuard)
   @Delete("/delete")
-  async deleteAccount(@Body() dto: UserLoginDto) {
+  async deleteAccount(@Headers() headers: any, @Body() dto: UserLoginDto) {
     const result = await this.userService.deleteAccount(dto);
     return JSON.parse(result);
   }
 
+  @UseGuards(AuthGuard)
   @Patch("/update")
   @HttpCode(HttpStatus.OK)
-  async updateUserInfo(@Body() dto: UserModifyDto): Promise<JSON> {
-    const result = await this.userService.update(dto);
+  async updateUserInfo(@Headers() headers: any, @Body() dto: UserModifyDto): Promise<JSON> {
+    const jwtToken = headers.authorization as string;
+    const result = await this.authApiService.update(dto, jwtToken);
     return JSON.parse(result);
   }
 }
--- be/src/auth-api/auth-api.service.ts
+++ be/src/auth-api/auth-api.service.ts
@@ -3,26 +3,45 @@ import {UserLoginDto} from "../user/dto/user-login.dto";
 import {UserService} from "../user/user.service";
 import {AuthService} from "../auth/auth.service";
 import {HttpResponse} from "../utils/http.response";
+import {CreateUserDto} from "../user/dto/create-user.dto";
+import {UserModifyDto} from "../user/dto/user-modify.dto";
 
 @Injectable()
 export class AuthApiService {
-    constructor(
-        private userService: UserService,
-        private authService: AuthService
-    ) {
-    }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
 
-    async login(dto: UserLoginDto): Promise<string> {
-        const user = await this.userService.login(dto);
-        const token = this.authService.issue(JSON.parse(user));
+  async register(dto: CreateUserDto): Promise<string> {
+    await this.userService.register(dto);
+    const body: HttpResponse = {
+      message: "회원가입 성공",
+      statusCode: 201,
+    };
 
-        const body: HttpResponse = {
-            message: "로그인 성공",
-            statusCode: 200,
-            data: {
-                token: token
-            },
-        };
-        return JSON.stringify(body);
-    }
+    return JSON.stringify(body);
+  }
+
+  async login(dto: UserLoginDto): Promise<string> {
+    const userUuid = await this.userService.login(dto);
+    const token = this.authService.issue(userUuid);
+
+    const body: HttpResponse = {
+      message: "로그인 성공",
+      statusCode: 200,
+      data: {
+        token: token,
+      },
+    };
+    return JSON.stringify(body);
+  }
+
+  delete() {}
+
+  async update(dto: UserModifyDto, jwtToken: string) {
+      const userUuid = this.authService.verify(jwtToken);
+      const result = await this.userService.update(userUuid, dto);
+      return JSON.stringify(result);
+  }
 }
