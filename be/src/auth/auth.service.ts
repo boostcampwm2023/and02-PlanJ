9caf1c4a249933eca2bd4ea8b9c815650e57820f
famo1245 <kandallee38@gmail.com>
2023-11-23 17:48:29 +0900

    Feat: add jwt verify
    JWT token 검증 method 추가

--- be/src/auth/auth.service.ts
+++ be/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import {Inject, Injectable} from '@nestjs/common';
+import {Inject, Injectable, UnauthorizedException} from '@nestjs/common';
 import {ConfigType} from "@nestjs/config";
 import * as jwt from "jsonwebtoken";
 import authConfig from "../config/auth.config";
@@ -9,13 +9,22 @@ export class AuthService {
         @Inject(authConfig.KEY) private config: ConfigType<typeof authConfig>
     ) {}
 
-    issue(userInfo: JSON) {
-        const payload = { userUuid: userInfo["data"]["userUuid"] };
+    issue(userUuid: string) {
+        const payload = { userUuid: userUuid };
+        const options = {
+            expiresIn: this.config.options.expiresIn,
+            issuer: this.config.options.issuer
+        }
+        return jwt.sign(payload, this.config.jwtSecret, options)
+    }
 
-        return jwt.sign(payload, this.config.jwtSecret, {
-            expiresIn: '1m',
-            audience: 'test.com',
-            issuer: 'test.com'
-        })
+    verify(jwtToken: string) {
+        try {
+            const payload = jwt.verify(jwtToken, this.config.jwtSecret) as (jwt.JwtPayload)
+            const { userUuid } = payload;
+            return userUuid;
+        } catch (e) {
+            throw new UnauthorizedException("유효하지 않은 사용자");
+        }
     }
 }
