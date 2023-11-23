36467a7dabec08a9360ff24c4fe3af8c1b727d16
Junsoo <junsoo2018@naver.com>
2023-11-23 16:52:50 +0900

    Feat: 일정 수정 시 입력받을 데이터 형식에 위치 정보 4가지 추가
    위치 정보 4가지(장소 이름, 주소, 위도, 경도)

--- be/src/schedule/dto/update-schedule.dto.ts
+++ be/src/schedule/dto/update-schedule.dto.ts
@@ -29,4 +29,24 @@ export class UpdateScheduleDto {
 
   @Matches(/^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})$/, { message: "올바른 날짜 및 시간 형식이 아닙니다." })
   endAt: string;
+
+  @IsString()
+  @IsOptional()
+  @Allow()
+  placeName: string | null;
+
+  @IsString()
+  @IsOptional()
+  @Allow()
+  placeAddress: string | null;
+
+  @IsString()
+  @IsOptional()
+  @Allow()
+  latitude: string | null;
+
+  @IsString()
+  @IsOptional()
+  @Allow()
+  longtitude: string | null;
 }
