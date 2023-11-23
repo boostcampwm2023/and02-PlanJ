fab1ebf32b46d3eefbac03f21642e184c41b1dc6
famo1245 <kandallee38@gmail.com>
2023-11-23 15:13:03 +0900

    Feat: add dependencies
    auth api module에서 필요한 module import

--- be/src/auth-api/auth-api.module.ts
+++ be/src/auth-api/auth-api.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AuthApiController } from "./auth-api.controller";
 import { UserModule } from "src/user/user.module";
+import {AuthModule} from "../auth/auth.module";
+import {AuthApiService} from "./auth-api.service";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
+  providers: [AuthApiService],
   controllers: [AuthApiController],
 })
 export class AuthApiModule {}
