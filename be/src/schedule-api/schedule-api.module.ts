d9ed29aeecc68c123b57598ef22fb0188142408c
Junsoo <junsoo2018@naver.com>
2023-11-23 13:10:54 +0900

    Fix: 빌드 오류 수정

--- be/src/schedule-api/schedule-api.module.ts
+++ be/src/schedule-api/schedule-api.module.ts
@@ -1,13 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ScheduleApiController } from "./schedule-api.controller";
 import { ScheduleModule } from "src/schedule/schedule.module";
-import { ScheduleMetaDataModule } from "src/schedule-meta-data/schedule-meta-data.module";
 import { UserModule } from "src/user/user.module";
 import { CategoryModule } from "src/category/category.module";
 import { ScheduleApiService } from "./schedule-api.service";
 
 @Module({
-  imports: [ScheduleModule, ScheduleMetaDataModule, CategoryModule, UserModule],
+  imports: [ScheduleModule, CategoryModule, UserModule],
   controllers: [ScheduleApiController],
   providers: [ScheduleApiService],
 })
