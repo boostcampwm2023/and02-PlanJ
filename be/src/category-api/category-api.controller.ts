7cf7ccc50aff67aa557a7e49d2df8132e42b8031
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-24 01:28:32 +0900

    Remove: remove unused import
    주석 처리된 명령어 삭제

--- be/src/category-api/category-api.controller.ts
+++ be/src/category-api/category-api.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Header, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Post } from "@nestjs/common";
 import { AddCategoryDto } from "src/category/dto/add-category.dto";
 import { CategoryApiService } from "./category-api.service";
 import { DeleteCategoryDto } from "src/category/dto/delete-category.dto";
@@ -8,7 +8,6 @@ export class CategoryApiController {
   constructor(private categoryApiService: CategoryApiService) {}
 
   @Post("/add")
-  // @Header("content-type", "application/json")
   async add(@Body() dto: AddCategoryDto): Promise<JSON> {
     const result = await this.categoryApiService.add(dto);
     return JSON.parse(result);
