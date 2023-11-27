b11558d9f860a9c3a729cfd92a716beaf176b615
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-27 23:05:31 +0900

    Refactor: relocate user_id column
    user_id column 위치 이동

--- be/src/schedule/entity/schedule-metadata.entity.ts
+++ be/src/schedule/entity/schedule-metadata.entity.ts
@@ -29,14 +29,14 @@ export class ScheduleMetadataEntity extends BaseEntity {
   @Column({ name: "end_time", type: "time" })
   endTime: string;
 
+  @Column({ name: "user_id", type: "int" })
+  userId: number;
+
   @Column({ name: "category_id", type: "int", nullable: true })
   categoryId: number;
 
   @DeleteDateColumn({ default: null, name: "deleted_at" })
   deletedAt: Date | null;
-
-  @Column({ name: "user_id", type: "int" })
-  userId: number;
 
   /*
    * relation
