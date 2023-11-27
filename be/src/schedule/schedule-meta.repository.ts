a0e8c2eb3c6f3e154165c8e5e706a2ee1e31df08
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-27 17:59:19 +0900

    Remove: remove redundant variable
    불필요한 변수 할당 삭제

--- be/src/schedule/schedule-meta.repository.ts
+++ be/src/schedule/schedule-meta.repository.ts
@@ -55,7 +55,6 @@ export class ScheduleMetaRepository extends Repository<ScheduleMetadataEntity> {
       await this.save(record);
       return record;
     } catch (error) {
-      console.log(error);
       throw new InternalServerErrorException();
     }
   }
@@ -64,28 +63,24 @@ export class ScheduleMetaRepository extends Repository<ScheduleMetadataEntity> {
     const todayStart = date.toString().split("T")[0] + "T00:00:00";
     const todayEnd = date.toString().split("T")[0] + "T23:59:59";
 
-    const founds = await this.createQueryBuilder("schedule_metadata")
+    return await this.createQueryBuilder("schedule_metadata")
       .leftJoinAndSelect("schedule_metadata.children", "schedule")
       .andWhere("schedule_metadata.user_id = :userId", { userId: user.userId })
       .andWhere("schedule.endAt BETWEEN :todayStart AND :todayEnd ", { todayStart, todayEnd })
       .getMany();
-
-    return founds;
   }
 
   async getAllScheduleByWeek(user: UserEntity, firstDay: Date, lastDay: Date): Promise<ScheduleMetadataEntity[]> {
     const weekStart = firstDay.toISOString().split("T")[0] + "T00:00:00";
     const weekEnd = lastDay.toISOString().split("T")[0] + "T23:59:59";
 
-    const founds = await this.createQueryBuilder("schedule_metadata")
+    return await this.createQueryBuilder("schedule_metadata")
       .leftJoinAndSelect("schedule_metadata.children", "schedule")
       .andWhere("schedule_metadata.user_id = :userId", { userId: user.userId })
       .andWhere("schedule.endAt BETWEEN :weekStart AND :weekEnd", { weekStart, weekEnd })
       .orWhere("schedule.startAt BETWEEN :weekStart AND :weekEnd", { weekStart, weekEnd })
       .orderBy("schedule.endAt")
       .getMany();
-
-    return founds;
   }
 
   async findByCategoryId(categoryId: number, userId: number) {
@@ -101,7 +96,6 @@ export class ScheduleMetaRepository extends Repository<ScheduleMetadataEntity> {
     try {
       await this.softDelete({ metadataId });
     } catch (error) {
-      console.log(error);
       throw new InternalServerErrorException();
     }
   }
