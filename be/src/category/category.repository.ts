0d806517762ae3e19795cab59196ee0e0c53ea3c
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-28 21:26:15 +0900

    Refactor: relocate validate entity
    entity가 null인지 검증하는 로직 service로 이동

--- be/src/category/category.repository.ts
+++ be/src/category/category.repository.ts
@@ -1,11 +1,6 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { CategoryEntity } from "./entity/category.entity";
 import { DataSource, Repository } from "typeorm";
-import { AddCategoryDto } from "./dto/add-category.dto";
-import { ulid } from "ulid";
-import { HttpResponse } from "src/utils/http.response";
-import { UserEntity } from "src/user/entity/user.entity";
-import { DeleteCategoryDto } from "./dto/delete-category.dto";
 
 @Injectable()
 export class CategoryRepository extends Repository<CategoryEntity> {
@@ -18,14 +13,7 @@ export class CategoryRepository extends Repository<CategoryEntity> {
       return null;
     }
 
-    const category = await this.findOne({
-      where: { categoryUuid: categoryUuid },
-    });
-
-    if (category === null) {
-      throw new NotFoundException("존재하지 않는 category uuid");
-    }
-    return category;
+    return await this.findOne({ where: { categoryUuid: categoryUuid } });
   }
 
   async findByUserId(userId: number) {
