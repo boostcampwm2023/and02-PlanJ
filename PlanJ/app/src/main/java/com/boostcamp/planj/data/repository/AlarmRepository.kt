733a2855fdcc135601e4440377baca9a7a00fd79
신유빈 <101437398+yubin0727@users.noreply.github.com>
2023-11-28 13:57:54 +0900

    Feat: 알람레포지토리 setAlarm() 구현
    schedule 객체를 받아 정보를 이용해 알람 생성

--- PlanJ/app/src/main/java/com/boostcamp/planj/data/repository/AlarmRepository.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/data/repository/AlarmRepository.kt
@@ -1,8 +1,10 @@
 package com.boostcamp.planj.data.repository
 
+import com.boostcamp.planj.data.model.Schedule
+
 interface AlarmRepository {
 
-    fun setAlarm()
+    fun setAlarm(schedule: Schedule)
 
     fun deleteAlarm(requestCode: Int)
 }
--- PlanJ/app/src/main/java/com/boostcamp/planj/data/repository/AlarmRepositoryImpl.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/data/repository/AlarmRepositoryImpl.kt
@@ -5,6 +5,9 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import com.boostcamp.planj.data.PlanJReceiver
+import com.boostcamp.planj.data.model.Repetition
+import com.boostcamp.planj.data.model.Schedule
+import java.util.Calendar
 import javax.inject.Inject
 
 class AlarmRepositoryImpl @Inject constructor(
@@ -19,20 +22,80 @@ class AlarmRepositoryImpl @Inject constructor(
         Intent(context, PlanJReceiver::class.java)
     }
 
-    override fun setAlarm() {
-        TODO("Not yet implemented")
+    override fun setAlarm(schedule: Schedule) {
+        schedule.alarm?.let { alarm ->
+            val requestCode = schedule.scheduleId.hashCode()
+            val title = schedule.title
+            val text = if (alarm.alarmType == "departure") {
+                "출발 시간 ${alarm.alarmTime}분 전입니다."
+            } else {
+                "종료 시간 ${alarm.alarmTime}분 전입니다."
+            }
+            val pendingIntent = createPendingIntent(requestCode, title, text)
+
+            val dateList = schedule.endTime.split("T", "/", ":")
+            val calendar = Calendar.getInstance()
+            calendar.set(
+                dateList[0].toInt(),
+                dateList[1].toInt() - 1,
+                dateList[2].toInt(),
+                dateList[3].toInt(),
+                dateList[4].toInt()
+            )
+            calendar.add(Calendar.MINUTE, -alarm.alarmTime)
+
+            if (schedule.repetition == null) {
+                setOnTimeAlarm(calendar, pendingIntent)
+            } else {
+                setRepeatAlarm(calendar, schedule.repetition, pendingIntent)
+            }
+        }
+    }
+
+    private fun setOnTimeAlarm(calendar: Calendar, pendingIntent: PendingIntent) {
+        alarmManager.set(
+            AlarmManager.RTC,
+            calendar.timeInMillis,
+            pendingIntent
+        )
+    }
+
+    private fun setRepeatAlarm(
+        calendar: Calendar,
+        repetition: Repetition,
+        pendingIntent: PendingIntent
+    ) {
+        val interval = if (repetition.cycleType == "DAILY") {
+            repetition.cycleCount
+        } else {
+            repetition.cycleCount * 7
+        }
+        alarmManager.setInexactRepeating(
+            AlarmManager.RTC,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY * interval,
+            pendingIntent
+        )
     }
 
     override fun deleteAlarm(requestCode: Int) {
         alarmManager.cancel(createPendingIntent(requestCode))
     }
 
-    private fun createPendingIntent(requestCode: Int): PendingIntent {
-        return PendingIntent.getBroadcast(
-            context,
-            requestCode,
-            alarmIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT
-        )
+    private fun createPendingIntent(
+        requestCode: Int,
+        title: String = "",
+        text: String = ""
+    ): PendingIntent {
+        return alarmIntent.let { intent ->
+            intent.putExtra("title", title)
+            intent.putExtra("text", text)
+            PendingIntent.getBroadcast(
+                context,
+                requestCode,
+                alarmIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        }
     }
 }
