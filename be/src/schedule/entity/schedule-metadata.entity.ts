a4c440e335798e1fc81c5c6406979f474a64a65c
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-28 21:12:36 +0900

    Feat: add attribute hasLocation
    해당 일정에 위치 정보가 있는지 확인하는 속성 추가

--- be/src/schedule/entity/schedule-metadata.entity.ts
+++ be/src/schedule/entity/schedule-metadata.entity.ts
@@ -42,6 +42,9 @@ export class ScheduleMetadataEntity extends BaseEntity {
   @Column({ type: "boolean", default: false })
   shared: boolean;
 
+  @Column({ type: "boolean", default: false })
+  hasLocation: boolean;
+
   @DeleteDateColumn({ default: null, name: "deleted_at" })
   deletedAt: Date | null;
 
