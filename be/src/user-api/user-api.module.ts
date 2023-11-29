2b88ed03be55ce3225820822882ec6a07b35a271
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-29 21:45:09 +0900

    Feat: add feature upload profile image
    사용자가 프로필 이미지를 업로드 할 수 있도록 기능 구현

--- be/src/user-api/user-api.module.ts
+++ be/src/user-api/user-api.module.ts
@@ -3,9 +3,10 @@ import { UserApiController } from "./user-api.controller";
 import { UserModule } from "src/user/user.module";
 import { AuthModule } from "../auth/auth.module";
 import { UserApiService } from "./user-api.service";
+import { ImageModule } from "../image/image.module";
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, ImageModule],
   providers: [UserApiService],
   controllers: [UserApiController],
 })
