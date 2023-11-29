143fa5624ca6b46f58dc8fe9fd163221dc93c679
Junsoo <junsoo2018@naver.com>
2023-11-29 18:47:11 +0900

    Feat: 이미 초대되어 있는 사용자임을 판별했을 때 일정 추가가 아닌 수정만 이루어지도록 구현

--- be/src/schedule/participate.repository.ts
+++ be/src/schedule/participate.repository.ts
@@ -38,7 +38,7 @@ export class ParticipateRepository extends Repository<ParticipantEntity> {
   // scheduleUuid에 연결된 schedulemetadataid가 이미 participant테이블의 author에 있는지 체크
   // 초대된 사용자가 새롭게 초대된 것인지 체크
   // 이미 있고 새롭게 초대된 것이 아니라면 add 필요없고 participant 테이블에 새로 만들 필요 없음
-  async isAlreadyInvited(authorMetadataId: number, invitedUserId: number): Promise<(number | boolean)[]> {
+  async isAlreadyInvited(authorMetadataId: number, invitedUserId: number): Promise<number[]> {
     const records = await this.createQueryBuilder("participant")
       .leftJoinAndSelect("participant.participant", "schedule_metadata")
       .where("participant.author_id = :authorId", { authorId: authorMetadataId })
@@ -47,11 +47,11 @@ export class ParticipateRepository extends Repository<ParticipantEntity> {
 
     console.log(records);
     if (records.length === 1) {
-      return [true, records[0].participantId];
+      return [1, records[0].participantId];
     }
 
     if (records.length === 0) {
-      return [false, null];
+      return [0, 0];
     }
   }
 }
