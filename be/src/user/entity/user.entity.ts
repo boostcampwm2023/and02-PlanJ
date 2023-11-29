38aae757666db01747ad79b8938adcab25e8ae40
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-29 18:19:31 +0900

    Feat: user image url attribute
    사용자 프로필 이미지 속성 추가

--- be/src/user/entity/user.entity.ts
+++ be/src/user/entity/user.entity.ts
@@ -39,6 +39,9 @@ export class UserEntity extends BaseEntity {
   @Column({ nullable: true, name: "accept_notification" })
   acceptNotification: boolean;
 
+  @Column({ nullable: true, default: null, length: 256, name: "profile_url" })
+  profileUrl: string;
+
   /*
    * relation
    * */
