551fa75f4415497b74872cdfbd2374eed091060c
윤찬 <vsvx13@naver.com>
2023-11-29 10:49:19 +0900

    Fix: temp 제거

--- PlanJ/app/src/main/java/com/boostcamp/planj/data/model/PostScheduleBody.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/data/model/PostScheduleBody.kt
@@ -8,15 +8,6 @@ data class PostScheduleBody(
     @SerializedName("categoryUuid") val categoryUuid : String,
     @SerializedName("title") val title : String,
     @SerializedName("endAt") val endAt : String,
-    @SerializedName("startLocation") val startLocation: TempLocation= TempLocation(),
-    @SerializedName("endLocation") val endLocation : TempLocation=TempLocation()
 )
 
-@Parcelize
-data class TempLocation(
-    val placeName : String="",
-    val address : String="",
-    val latitude : Double=23.123,
-    val longitude : Double=23.123
-) : Parcelable
 
