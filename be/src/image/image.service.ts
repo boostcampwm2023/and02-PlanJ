dedfa8a6e8b9c2a70d9871785baa63200e2da946
JIYOUNG KIM <kandallee38@gmail.com>
2023-11-29 21:43:51 +0900

    Feat: add feature upload image
    ncp object storage에 사용자의 프로필 이미지를 추가하기 위한 서비스 추가
    추후 이미지 검열, 사이즈 조정 기능 이용 예정

---
+++ be/src/image/image.service.ts
@@ -0,0 +1,48 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { ConfigType } from "@nestjs/config";
+import awsConfig from "../config/aws.config";
+import * as AWS from "aws-sdk";
+import { ulid } from "ulid";
+
+@Injectable()
+export class ImageService {
+  private s3: AWS.S3;
+
+  constructor(@Inject(awsConfig.KEY) private config: ConfigType<typeof awsConfig>) {
+    this.s3 = new AWS.S3({
+      endpoint: new AWS.Endpoint(config.endPoint as string),
+      region: config.region as string,
+      credentials: {
+        accessKeyId: config.accessKey as string,
+        secretAccessKey: config.secretKey as string,
+      },
+    });
+  }
+
+  async uploadImage(profileImage: Express.Multer.File) {
+    const { buffer, originalname } = profileImage;
+    const date = new Date();
+    const [, extension] = originalname.split(".");
+    const fileName = ulid();
+    const directoryPath = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}/`;
+    const filePath = directoryPath + `${fileName}.${extension}`;
+
+    try {
+      await this.s3
+        .putObject({
+          Bucket: this.config.bucket as string,
+          Key: filePath,
+          Body: buffer,
+          ACL: "public-read",
+        })
+        .promise();
+      return this.getProfileImageUrl(filePath);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  private getProfileImageUrl(filePath: string) {
+    return this.config.endPoint + "/" + filePath;
+  }
+}
