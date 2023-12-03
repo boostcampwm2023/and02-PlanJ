7d1bee29e6fd28abb06ba799bba0f7635165a776
신유빈 <101437398+yubin0727@users.noreply.github.com>
2023-12-04 03:15:02 +0900

    Feat: 알림 권한 허용 요청 추가

--- PlanJ/app/src/main/java/com/boostcamp/planj/ui/main/MainActivity.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/ui/main/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.boostcamp.planj.ui.main
 
-import android.content.Intent
+import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.boostcamp.planj.BuildConfig
 import com.boostcamp.planj.R
 import com.boostcamp.planj.data.model.Category
 import com.boostcamp.planj.databinding.ActivityMainBinding
@@ -36,6 +37,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        checkPermission()
         setJetpackNavigation()
         floatingButtonVisibleListener()
         setListener()
@@ -48,6 +51,40 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+            && PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            )
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, resources.getString(R.string.post_notifications_allowed), Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, resources.getString(R.string.post_notifications_denied), Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
     }
 
     private fun setListener() {
@@ -80,5 +117,7 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.setupWithNavController(navController)
     }
 
-
+    companion object{
+        val REQUEST_CODE = 100
+    }
 }
