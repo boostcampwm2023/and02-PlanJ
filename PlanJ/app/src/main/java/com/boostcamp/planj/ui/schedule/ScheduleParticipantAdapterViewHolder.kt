f07c684833d618b12bdfa155015a983278352cb6
신유빈 <101437398+yubin0727@users.noreply.github.com>
2023-12-06 16:47:32 +0900

    Refactor: 리사이클러뷰 바인딩으로 변경

--- PlanJ/app/src/main/java/com/boostcamp/planj/ui/schedule/ScheduleParticipantAdapterViewHolder.kt
+++ PlanJ/app/src/main/java/com/boostcamp/planj/ui/schedule/ScheduleParticipantAdapterViewHolder.kt
@@ -16,27 +16,9 @@ class ScheduleParticipantAdapterViewHolder(private val binding: ItemParticipantB
         listener: ScheduleParticipantsListener
     ) {
         binding.participant = friendState.participant
+        binding.isEditMode = isEditMode
+        binding.isParticipated = friendState.isParticipated
 
-        if (isEditMode) {
-            binding.layoutParticipant.visibility = View.VISIBLE
-            binding.cbParticipantCheck.isChecked = friendState.isParticipated
-            if (friendState.participant.currentUser) binding.cbParticipantCheck.isEnabled = false
-            binding.cbParticipantCheck.visibility = View.VISIBLE
-            binding.imgParticipantDone.visibility = View.GONE
-            binding.imgParticipantNotDone.visibility = View.GONE
-        } else {
-            if (!friendState.isParticipated) binding.layoutParticipant.visibility = View.GONE
-            else {
-                binding.cbParticipantCheck.visibility = View.GONE
-                if (friendState.participant.isFinished) {
-                    binding.imgParticipantDone.visibility = View.VISIBLE
-                    binding.imgParticipantNotDone.visibility = View.GONE
-                } else {
-                    binding.imgParticipantDone.visibility = View.GONE
-                    binding.imgParticipantNotDone.visibility = View.VISIBLE
-                }
-            }
-        }
         binding.cbParticipantCheck.setOnCheckedChangeListener { _, isChecked ->
             listener.onCheckedChanged(isChecked, friendState.participant)
         }
